13949ff90da445019502313e089d156a7dcdc11a
pgeorge14 <77898807+pgeorge14@users.noreply.github.com>
2023-03-25 12:11:02 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -28,7 +28,8 @@ mapper2 = plt.cm.ScalarMappable(norm=norm, cmap=my_dullmap)
 st.header('Percentile Dashboard App')
 st.write('by @analyticswba')
 
-st.write('\n\n\nPlease fill out the parameters and click run to see your visualisation\n\nIt is recommended to set minimum number of 90s to at least a third of the bar')
+st.write("\n\n\nA tool to show performance levels for any outfield player in England's top four leagues over the past 3 seasons.")
+st.write('\n\nPlease set the parameters and click run to see your visualisation.\n\nIt is recommended to set minimum number of 90s to at least a third of the bar.')
 
 season  = st.selectbox('Select season', options = ['2022/23','2021/22','2020/21'])
 
